dbe6a1c05182316cb2cd8ca3b48dae0a45b7596b
Anna Gilbert <akgilbert@users.noreply.github.com>
2018-12-19 09:39:38 -0500

    Create general_assembly_training_sql_day2.sql

---
+++ general_assembly_training_sql_day2.sql
@@ -0,0 +1,35 @@
+--Joins Guided Practice
+
+--Our first join :)
+SELECT 
+	b.store, 
+	a.item, 
+	a.total								
+FROM 
+	sales a									
+INNER JOIN stores b
+
+	ON a.store = b.store;
+
+
+--Use Inner to Connect Sales to Products
+--Planning: Connecting field: sales.item and products.item_no
+
+--Variation 1:
+select 
+*
+from sales
+inner join products
+on sales.item = products.item_no
+limit 10 ;
+
+--Variation 2 with specific column names and aliases:
+select 
+p.item_no
+, p.item_description
+, s.total
+from sales s
+inner join products p
+on s.item = p.item_no
+limit 1000 ;
+
