bb11a43a965bba88ed66ac81ecd50ca9dfacb827
Anna Gilbert <akgilbert@users.noreply.github.com>
2018-12-20 11:46:35 -0500

    Update general_assembly_sql_day3.sql

--- general_assembly_sql_day3.sql
+++ general_assembly_sql_day3.sql
@@ -87,8 +87,119 @@ FROM sales
 LIMIT 100
 ;
 
+                  
+--date functions!
+                  
+--current_date
+--extract(date_part from field1)
+--age
+
+--what year is the data set from?
+select distinct extract(year from date) as year
+from sales 
+limit 10 ; 
+
+--what about the months?
+select distinct 
+extract(year from date) as year
+, extract(month from date) as month
+from sales 
+order by 1,2 ; 
+
+--what is the current date?
+--select 1 ; 
+--select 'quick fox' ; 
+select current_date ; 
+
+--what is yesterday's date?
+select current_date - 1 as yesterday ;  
+
+--what month are we in?
+select extract(month from current_date) ; 
+
+--age sample
+select date 
+, current_date
+, age(current_date, date)
+from sales 
+limit 10 ; 
+
+                  
+--case statements!
+
+---what is the averages sales of products with fruit in their name (apple, berry, or orange) 
+	---vs no fruit in their name?
+
+SELECT 
+CASE WHEN description ILIKE '%Apple%' OR description ILIKE '%Berry%' 
+			OR description ILIKE '%Orange%' OR description ILIKE '%Pear%'  
+		THEN 'Fruit Product'
+	ELSE 'Not Fruit Product'
+END AS fruit_category
+, ROUND(AVG(total),2) as avg_sales
+, COUNT(*) as sales_count
+FROM sales
+GROUP BY 1
+LIMIT 100 ; 
 
 
+SELECT 
+CASE WHEN description ILIKE '%Apple%' OR description ILIKE '%Berry%' 
+			OR description ILIKE '%Orange%' OR description ILIKE '%Pear%'  
+		THEN 'Fruit Product'
+	ELSE 'Not Fruit Product'
+END AS fruit_category
+, description
+, avg(total)
+FROM sales
+group by 1,2
+LIMIT 100 ; 
+
+
+
+
+
+---what is the average sales in large, small and medium counties in iowa?
+SELECT *
+FROM counties
+;
+
+--large: > 100000
+--medium: > 10000
+--small: everything else, <= 10000
+
+SELECT
+CASE WHEN population > 100000
+	THEN 'Large'
+WHEN population > 10000
+	THEN 'Medium'
+ELSE 'Small'
+END AS county_size
+, *
+FROM counties
+;
+
+SELECT *
+FROM sales s
+INNER JOIN counties c
+USING (county)
+LIMIT 10 ; 
+
+
+SELECT 
+CASE WHEN population > 100000
+	THEN 'Large'
+WHEN population > 10000
+	THEN 'Medium'
+ELSE 'Small'
+END AS county_size
+, round(avg(total),2) as average_sales
+FROM sales s
+INNER JOIN counties c
+USING (county)
+GROUP BY 1
+ORDER BY 2 desc
+LIMIT 10 ; 
 
 
 
